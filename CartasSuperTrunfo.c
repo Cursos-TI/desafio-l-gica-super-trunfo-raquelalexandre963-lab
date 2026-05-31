4d78b912e49955dd965ae8d8f98141188a53422a
Alexandre <teu@email.com>
2026-05-31 11:01:26 -0300

    fiz uma alteracao no codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,14 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+printf("fiz o desafio de novato\n");
+printf("fiz o desafio de cafrastar carta\n");
 
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
+printf("ola mundo no codespace\n");
+printf("exemplo de aplicacao de codigo\n");
 
 return 0;
 } 

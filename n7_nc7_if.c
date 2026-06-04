c756ddc109f8b4e3cc4892e48dd0b5990ea8d3a1
Alexandre <teu@email.com>
2026-06-04 20:36:41 -0300

    exercicio pronto

---
+++ n7_nc7_if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main() {
+        int numero1, numero2;
+
+        numero1 = 10;
+        numero2 = 5;
+
+        if(numero1 > numero2) {
+            printf("O numero 1 é maior que o numero 2\\n");
+        
+        }
+
+        printf("Fora do if\\n");
+
+        return 0;
+    }

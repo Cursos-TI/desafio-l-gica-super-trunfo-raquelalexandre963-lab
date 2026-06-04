6453500204160243df4741bf738fd3be0727490b
Alexandre <teu@email.com>
2026-06-04 08:43:02 -0300

    fechei essa parte do exercicos

---
+++ n5_nc5_DEV.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    float y = 5.0;
+    char c = 'a';
+
+    printf("x == y: %d\n", x >= y);
+    printf("x == y: %d\n", x == y);
+    printf("x != y: %d\n", x != y);
+    pritf("o valor ascil de %c é %d:  \n", c, c);
+
+    return 0;
+}
